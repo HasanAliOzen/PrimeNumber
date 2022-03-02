be32be28613ed9ec571768ae104f3857ce18a87d
Hasan Ali <hasanali.ozen@outlook.com>
2022-03-02 10:13:51 +0300

    v1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//  Define a PrimeNumber class. In this class:
+// -The inputNumber function should provide a value to the number member variable from the
+//  keyboard.
+// -The isPrime function should return the information whether the number member variable is
+//  prime or not.
+// -closesetPrimeNumber function should return the largest prime number closest to the object
+//  member
+
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class PrimeNumber
+    {
+        private int number;
+
+        public inputNumber()
+        {
+            System.Console.WriteLine("Enter a number : ");
+            number = Int.parse(Console.ReadLine());
+        }
+
+        public isPrime() { }
+    }
+}
